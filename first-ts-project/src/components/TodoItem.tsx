b84b5b2f0638abe4f97a30164d020c39643c4d06
Philip Zimmermann <philip.zimmermann@freenet.de>
2023-03-29 21:51:08 +0200

    change styles name in TodoItem

--- first-ts-project/src/components/TodoItem.tsx
+++ first-ts-project/src/components/TodoItem.tsx
@@ -2,6 +2,6 @@ import React from "react";
 import styles from "./TodoItem.module.css";
 
 const TodoItem: React.FC<{ text: string }> = (props) => {
-  return <li className={styles.todos}>{props.text}</li>;
+  return <li className={styles.item}>{props.text}</li>;
 };
 export default TodoItem;
