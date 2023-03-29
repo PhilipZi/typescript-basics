1dc3cc9c6a9d3c2f24a7f84b29d75fc6cab5a022
Philip Zimmermann <philip.zimmermann@freenet.de>
2023-03-29 18:58:42 +0200

    learning about array and objects in ts

--- basics.ts
+++ basics.ts
@@ -15,3 +15,28 @@ userName = "Max";
 let isInstructor: boolean;
 
 isInstructor = true;
+
+// More complex types
+
+let hobbies: string[];
+
+hobbies = ["sport", "cooking"];
+
+let person: {
+  name: string;
+  age: number;
+};
+
+person = {
+  name: "Max",
+  age: 21,
+};
+
+// person = {
+//   isEmployee: true,
+// };
+
+let people: {
+  name: string;
+  age: number;
+}[];
