9bbe848a23055843fc3ad42bcb6871f05ac1e656
Philip Zimmermann <philip.zimmermann@freenet.de>
2023-03-29 19:38:27 +0200

    learn about generics

--- basics.ts
+++ basics.ts
@@ -50,3 +50,15 @@ function add(a: number, b: number) {
 function printOutput(value: any) {
   console.log(value);
 }
+
+// Generics
+
+function insertAtBeginning<T>(array: T[], value: T) {
+  const newArray = [value, ...array];
+  return newArray;
+}
+
+const demoArray = [1, 2, 3];
+
+const updatedArray = insertAtBeginning(demoArray, -1); // [-1,1,2,3]
+const stringArray = insertAtBeginning(["a", "b", "c"], "d"); // [d, a, b, c]
